9d1f687218614a6175cd988420c1ed42df2ddb80
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-02-11 13:54:05 +0100

    added getter and sett and contrsuc

--- src/main/java/com/AirBnb/TimberAndStone/models/Period.java
+++ src/main/java/com/AirBnb/TimberAndStone/models/Period.java
@@ -11,4 +11,23 @@ public class Period {
 
     @NotNull(message = "endDate cant be null")
     private LocalDate endDate;
+
+    public Period() {
+    }
+
+    public @NotNull(message = "startDate cant be null") LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(@NotNull(message = "startDate cant be null") LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public @NotNull(message = "endDate cant be null") LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(@NotNull(message = "endDate cant be null") LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
