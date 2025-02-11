80446f4d2f7e1be2a9e6542c01b1a239e794c457
SatangSagwa <emilia.ivarsson@live.se>
2025-02-11 16:15:33 +0100

    added annotation and id

--- src/main/java/com/AirBnb/TimberAndStone/models/Rental.java
+++ src/main/java/com/AirBnb/TimberAndStone/models/Rental.java
@@ -1,12 +1,21 @@
 package com.AirBnb.TimberAndStone.models;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Document
 public class Rental {
+
+    @Id
+    private String id;
 
     @NotNull(message = "Title can not be null")
     @NotEmpty(message = "Title can not be empty")
