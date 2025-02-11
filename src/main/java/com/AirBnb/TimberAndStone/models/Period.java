cb6784ab2ae25fe3eef0075f1b6303b58600d17d
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-02-11 15:09:19 +0100

    punk test för application.yaml

--- src/main/java/com/AirBnb/TimberAndStone/models/Period.java
+++ src/main/java/com/AirBnb/TimberAndStone/models/Period.java
@@ -6,28 +6,28 @@ import java.time.LocalDate;
 
 public class Period {
 
-    @NotNull(message = "startDate cant be null")
+    @NotNull(message = "startDate cant be null.")
     private LocalDate startDate;
 
-    @NotNull(message = "endDate cant be null")
+    @NotNull(message = "endDate cant be null.")
     private LocalDate endDate;
 
     public Period() {
     }
 
-    public @NotNull(message = "startDate cant be null") LocalDate getStartDate() {
+    public @NotNull(message = "startDate cant be null.") LocalDate getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(@NotNull(message = "startDate cant be null") LocalDate startDate) {
+    public void setStartDate(@NotNull(message = "startDate cant be null.") LocalDate startDate) {
         this.startDate = startDate;
     }
 
-    public @NotNull(message = "endDate cant be null") LocalDate getEndDate() {
+    public @NotNull(message = "endDate cant be null.") LocalDate getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(@NotNull(message = "endDate cant be null") LocalDate endDate) {
+    public void setEndDate(@NotNull(message = "endDate cant be null.") LocalDate endDate) {
         this.endDate = endDate;
     }
 }
