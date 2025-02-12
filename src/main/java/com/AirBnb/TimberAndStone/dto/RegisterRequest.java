1c4af9b97900831d4af36959b3e5295681600b71
SatangSagwa <emilia.ivarsson@live.se>
2025-02-12 12:13:07 +0100

    comment cleanup

--- src/main/java/com/AirBnb/TimberAndStone/dto/RegisterRequest.java
+++ src/main/java/com/AirBnb/TimberAndStone/dto/RegisterRequest.java
@@ -148,30 +148,4 @@ public class RegisterRequest {
         return roles;
     }
 
-    /*
-    @NotBlank
-    private String username;
-    @NotBlank
-    private String password;
-    private Set<Role> roles;
-
-    public RegisterRequest(String username, String password, Set<Role> roles) {
-        this.username = username;
-        this.password = password;
-        this.roles = roles;
-    }
-
-    public @NotBlank String getUsername() {
-        return username;
-    }
-
-    public @NotBlank String getPassword() {
-        return password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-     */
 }
