de28d79b7f62636c3a2837b3e257bb4e0a7c0f34
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-02-14 21:09:32 +0100

    added HttpMessageNotReadableException for handling when you try to input wrong ENUM

--- src/main/java/com/AirBnb/TimberAndStone/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/AirBnb/TimberAndStone/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.AirBnb.TimberAndStone.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -52,5 +53,12 @@ public class GlobalExceptionHandler {
                 .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
                 .collect(Collectors.joining(", "));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessages);
+    }
+
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The request body is invalid.");
+
     }
 }
