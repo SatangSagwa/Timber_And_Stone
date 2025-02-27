cc8109fd71f002c2215da4a2ba869b04bc84761c
SatangSagwa <emilia.ivarsson@live.se>
2025-02-27 16:49:47 +0100

    Added validation in for booking to check for bookingnumber-uniqueness in combination with rental and user.

--- src/main/java/com/AirBnb/TimberAndStone/repositories/BookingRepository.java
+++ src/main/java/com/AirBnb/TimberAndStone/repositories/BookingRepository.java
@@ -1,6 +1,8 @@
 package com.AirBnb.TimberAndStone.repositories;
 
 import com.AirBnb.TimberAndStone.models.Booking;
+import com.AirBnb.TimberAndStone.models.Rental;
+import com.AirBnb.TimberAndStone.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
@@ -8,5 +10,9 @@ import java.util.List;
 public interface BookingRepository extends MongoRepository<Booking, String> {
     List<Booking> findByUserId(String userId);
     List<Booking> findByRentalId(String rentalId);
+    // find by booking number should be swapped for findByBookingNumberAndUserAndRental since
+    // booking number is not globally unique and may return
+    // multiple results and throw IncorrectResultSizeDataAccessException.
     Booking findByBookingNumber(String bookingId);
+    Booking findByBookingNumberAndUserAndRental(String bookingNumber, User user, Rental rental);
 }
