ea2e802d7643747c50d66bca4e0ba56c5d497a95
SatangSagwa <emilia.ivarsson@live.se>
2025-02-28 17:15:44 +0100

    Changed validateRentalReviewRequest to instead take RentalReview as a parameter to avoid double vaildation.

--- src/main/java/com/AirBnb/TimberAndStone/services/RentalReviewService.java
+++ src/main/java/com/AirBnb/TimberAndStone/services/RentalReviewService.java
@@ -111,7 +111,7 @@ updateRentalRating
                 .orElseThrow(() -> new ResourceNotFoundException("Rental review not found"));
 
         // Validate the rating and review inputs before proceeding
-        validateRentalReviewRequest(request, id);
+        validateRentalReviewRequest(request, existingRentalReview);
 
         // find booking
         Booking booking = bookingRepository.findByBookingNumberAndUserAndRental(
@@ -178,11 +178,13 @@ updateRentalRating
          */
     }
 
-    private void validateRentalReviewRequest(PatchRentalReviewRequest rentalReviewRequest, String id) {
+    private void validateRentalReviewRequest(PatchRentalReviewRequest rentalReviewRequest, RentalReview review) {
 
+        /*
         RentalReview review = rentalReviewRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Booking not found2"));
 
+         */
         if(rentalReviewRequest.getRating() != null) {
             if(rentalReviewRequest.getRating() < 1 || rentalReviewRequest.getRating() > 5) {
                 throw new IllegalArgumentException("Rating has to be between 1 and 5");
