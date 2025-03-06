62c5c94f33a8c9c4d4b3a11cd5145b3197c992aa
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-03-06 17:54:33 +0100

    added PatchRequest for userRevidw

---
+++ src/main/java/com/AirBnb/TimberAndStone/requests/userReview/PatchUserReviewRequest.java
@@ -0,0 +1,18 @@
+package com.AirBnb.TimberAndStone.requests.userReview;
+
+public class PatchUserReviewRequest {
+
+    private Integer rating;
+    private String review;
+
+    public PatchUserReviewRequest() {
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public String getReview() {
+        return review;
+    }
+}
